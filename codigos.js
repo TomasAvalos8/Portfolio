be7a58b2c455ba796c4fa2394fd5e36300d957ea
Tomas Avalos <tomasgonzaloavalos@gmail.com>
2025-07-30 22:30:36 -0300

    Se agregan varios funcionamientos y diseños nuevos y optimizados. Falta la muestra de proyectos y conexion del formulario.

--- codigos.js
+++ codigos.js
@@ -1,3 +1,9 @@
 function Redes(url ){
     window.open(url);
 }
+
+function copiarTexto(texto) {
+    navigator.clipboard.writeText(texto)
+      .then(() => alert(`Copiado: ${texto}`))
+      .catch(err => alert("Error al copiar"));
+}
